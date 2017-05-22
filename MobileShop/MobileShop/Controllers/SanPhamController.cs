3c447a3273f2576de12e7ddd564962a6de424228
DESKTOP-VQM4KIO\ASUS <mychikhtn@gmail.com>
2017-05-22 21:09:59 +0700

    Action: MainLayout and DanhSachSanPham

---
+++ MobileShop/MobileShop/Controllers/SanPhamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MobileShop.Models.BUS;
+
+
+
+namespace MobileShop.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            var dsSanPham = SanPhamBUS.DanhSach();
+            return View(dsSanPham);
+        }
+    }
+}
---
+++ MobileShop/MobileShop/Models/BUS/SanPhamBUS.cs
@@ -0,0 +1,18 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Models.BUS
+{
+    public class SanPhamBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new MobileShopConnectionDB();
+            return db.Query<SanPham>("Select * from SanPham where BiXoa!=1");
+        }
+       
+    }
+}
